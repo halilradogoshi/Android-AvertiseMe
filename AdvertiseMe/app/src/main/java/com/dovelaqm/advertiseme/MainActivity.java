9726888535256fed25f2a42263f35b089be16f7f
Halil Radogoshi <halilradogoshi@gmail.com>
2016-01-31 00:57:28 +0100

    Created methods for call and switch views
    Next to create code to save and read data

--- AdvertiseMe/app/src/main/java/com/dovelaqm/advertiseme/MainActivity.java
+++ AdvertiseMe/app/src/main/java/com/dovelaqm/advertiseme/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dovelaqm.advertiseme;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
     }
 
     @Override
@@ -26,6 +29,29 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    //Method to save data and switch between the visibility of layers
+    private void saveAdvertisement(View view){
+        findViewById(R.id.layout_content).setVisibility(view.GONE);
+        findViewById(R.id.layout_overview).setVisibility(view.VISIBLE);
+    }
+
+    //Method to make the phone call
+    private void callMe(){
+        //Get the phone number from the field Phone
+        String phoneNumber = findViewById(R.id.editText_phone2).toString().replace("-","");
+
+        //Start the phone feature
+        Intent phoneIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
+        try{
+            startActivity(phoneIntent);
+        }catch(android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getApplicationContext(),"You might not be able to make phone call on this device", Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 
     @Override
