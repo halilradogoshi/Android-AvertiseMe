bad3be0ac9cd1e3177f57bd9538862e3eee7f780
Halil Radogoshi <halilradogoshi@gmail.com>
2016-01-31 02:06:13 +0100

    Small changes

--- AdvertiseMe/app/src/main/java/com/dovelaqm/advertiseme/MainActivity.java
+++ AdvertiseMe/app/src/main/java/com/dovelaqm/advertiseme/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dovelaqm.advertiseme;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,18 +11,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Declare the EditText fields and SharePreferences
+
+    EditText editText_title;
+    EditText editText_title2;
+    EditText editText_name;
+    EditText editText_name2;
+    EditText editText_text;
+    EditText editText_text2;
+    EditText editText_phone;
+    EditText editText_phone2;
+    EditText editText_price;
+    EditText editText_price2;
+
+    public static final String MY_PREFERENCES = "MyPrefsFile";
+    public static final String TITLE = "titleKey";
+    public static final String NAME = "nameKey";
+    public static final String TEXT = "textKey";
+    public static final String PHONE = "phoneKey";
+    public static final String PRICE = "priceKey";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+       // setSupportActionBar(toolbar);
     }
 
     @Override
@@ -31,28 +54,38 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    //Method to save data and switch between the visibility of layers
-    private void saveAdvertisement(View view){
-        findViewById(R.id.layout_content).setVisibility(view.GONE);
-        findViewById(R.id.layout_overview).setVisibility(view.VISIBLE);
+    //Method to save data and switch between the visibility of layouts
+    private void saveAdvertisement(){
+
+
+        //Switch the visibility of layouts
+        findViewById(R.id.layout_content).setVisibility(View.GONE);
+        findViewById(R.id.layout_overview).setVisibility(View.VISIBLE);
     }
 
-    //Method to make the phone call
-    private void callMe(){
-        //Get the phone number from the field Phone
-        String phoneNumber = findViewById(R.id.editText_phone2).toString().replace("-","");
+    //Method to make the phone call. Do not forget to add the permission to call on the manifest
+    private void callMe(View view){
+        //Get the phone number from the relevant field on the overview_main
+        String phoneNumber = findViewById(R.id.editText_phone2).toString().replace("-","");         //remove possible minus signs from the string
 
-        //Start the phone feature
+        //Start the phone activity
         Intent phoneIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phoneNumber));
         try{
             startActivity(phoneIntent);
         }catch(android.content.ActivityNotFoundException ex) {
             Toast.makeText(getApplicationContext(),"You might not be able to make phone call on this device", Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+        //Method to read the data
+        private void readAdvertisement(View view){
 
 
 
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
